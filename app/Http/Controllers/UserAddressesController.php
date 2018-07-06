f10e7fcc53ace23c1bd5aa8ac560f4ed9c7470f5
vagrant <vagrant@homestead>
2018-07-06 08:31:56 +0000

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -33,6 +33,12 @@ class UserAddressesController extends Controller
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
     }
 
+    /**
+     * 保存收货地址
+     *
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -46,5 +52,57 @@ class UserAddressesController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * 编辑收货地址
+     *
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    /**
+     * 处理收货地址编辑
+     *
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * 删除收货地址
+     *
+     * @param UserAddress $user_address
+     * @return array
+     * @throws \Exception
+     */
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+
+        return [];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,12 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
         //用户创建收货地址处理
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+        //收货地址编辑
+        Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+        //收货地址修改处理
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        //收货地址删除
+        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     });
     // 结束
 });
