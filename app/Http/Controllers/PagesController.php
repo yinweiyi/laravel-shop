5450a37447aad5f9c48513d439017890131344ca
vagrant <vagrant@homestead>
2018-07-06 03:31:49 +0000

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,9 +7,24 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
 
-    //首页展示
+    /**
+     * 首页展示
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function root()
     {
         return view('pages.root');
+    }
+
+    /**
+     * 验证邮箱页
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
     }
 }
