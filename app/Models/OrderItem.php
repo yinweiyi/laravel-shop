09ec0dbcabedc624a11ce17bc1c98ac013b147c8
Wayee <378772944@qq.com>
2018-07-09 09:01:38 +0000

    订单模型

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = ['amount', 'price', 'rating', 'review', 'reviewed_at'];
+    protected $dates = ['reviewed_at'];
+    public $timestamps = false;
+
+    /**
+     * 关联商品表
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * 关联商品sku
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function productSku()
+    {
+        return $this->belongsTo(ProductSku::class);
+    }
+
+    /**
+     * 关联订单
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
