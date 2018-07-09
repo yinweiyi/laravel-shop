8e1423a8811d2b6d947be2a14821e251da798e43
Wayee <378772944@qq.com>
2018-07-09 04:07:38 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -41,11 +41,21 @@ class ProductsController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('编辑商品');
 
             $content->body($this->form()->edit($id));
         });
+    }
+
+    /**
+     * Update interface.
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function update($id)
+    {
+        return $this->form()->update($id);
     }
 
     /**
@@ -57,11 +67,20 @@ class ProductsController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('创建商品');
 
             $content->body($this->form());
         });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store()
+    {
+        return $this->form()->store();
     }
 
     /**
@@ -84,9 +103,8 @@ class ProductsController extends Controller
             $grid->review_count('评论数');
 
             $grid->actions(function ($actions) {
-                $actions->disabledDelete();
+                $actions->disableDelete();
             });
-
             $grid->tools(function ($tools) {
                 // 禁用批量删除按钮
                 $tools->batch(function ($batch) {
@@ -105,10 +123,30 @@ class ProductsController extends Controller
     {
         return Admin::form(Product::class, function (Form $form) {
 
-            $form->display('id', 'ID');
+            // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+            $form->text('title', '商品名称')->rules('required');
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            // 创建一个选择图片的框
+            $form->image('image', '商品图片')->rules('required|image');
+
+            // 创建一个富文本编辑器
+            $form->editor('description', '商品描述')->rules('required');
+
+            // 创建一组单选框
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
+
+            // 直接添加一对多的关联模型
+            $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
+                $form->text('title', 'SKU 名称')->rules('required');
+                $form->text('description', 'SKU 描述')->rules('required');
+                $form->text('price', '单价')->rules('required|numeric|min:0.01');
+                $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+            });
+
+            // 定义事件回调，当模型即将保存时会触发这个回调
+            $form->saving(function (Form $form) {
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+            });
         });
     }
 }
