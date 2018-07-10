3408d53c53c9c0f34456ce034e0f96878fcbf5fa
Wayee <378772944@qq.com>
2018-07-10 06:38:23 +0000

    集成支付宝

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,13 @@ Route::group(['middleware' => 'auth'], function() {
     });
     // 结束
 });
+Route::get('alipay', function() {
+    return app('alipay')->web([
+        'out_trade_no' => time(),
+        'total_amount' => '1',
+        'subject' => 'test subject - 测试',
+    ]);
+});
 
 //商品详情页
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
